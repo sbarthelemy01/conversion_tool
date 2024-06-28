625ba657da137496f4d4261d8ccd70fd4898c848
sambarthelemy <sbarthelemy@ufl.edu>
2024-06-27 23:58:36 -0400

    implemented api call to convert currencies, result comes as undefined (will redo)

--- src/components/currency/Currency.jsx
+++ src/components/currency/Currency.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import axios from 'axios';
+import $ from 'jquery';
 import Select from 'react-select'; //for use in creating searchable dropdowns
 import IconButton from '@mui/material/IconButton';
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import {Countries} from './Countries.js';
 import "./Currency.css";
+
 
 /*
 Source help for searchable dropdowns:
@@ -11,114 +14,159 @@ https://react-select.com/home
 https://www.digitalocean.com/community/tutorials/react-react-select
 https://www.dhiwise.com/post/how-to-implement-a-react-search-bar-with-dropdown 
 
+API source help: 
+API key -> https://openexchangerates.org/api/latest.json?app_id=54f59ce0f37d4c6eaf2972a402446648
+https://dev.to/fleepgeek/react-throwaway-app-1-currency-converter--52c7 
+https://blog.logrocket.com/build-dynamic-currency-converter-ecommerce-react-app/ 
 */
 
-const Currency = () => {
+const api_key = 'wAMerLDGEZqcmLqpUeQP7EjyOHQ1YbyA';
+const endpoint = 'live';
+class Currency extends Component {
 
-	const [country1, setCountry1] = useState(null); 
-	const [country2, setCountry2] = useState(null);
+	state = {
+        result: "",
+        fromCurrency: "",
+        toCurrency: "",
+        amount: "",
+        currencies: [],
 
-	const [input, setInput] = useState(''); 
-	let [output, setOutput] = useState('');
+    };
 
+    
+	componentDidMount() {
+        axios
+            //.get("https://openexchangerates.org/api/latest.json?app_id=54f59ce0f37d4c6eaf2972a402446648")
+			.get('https://api.currencybeacon.com/v1/latest?api_key=' + api_key)
+            .then(response => {
+                
+                const currencyArray = ["USD"] //initializing currencies from api starting with the base currency (USD)
+                for (const key in response.data.rates) {
+                    currencyArray.push(key)
+                }
+                this.setState({ currencies: currencyArray.sort() })
+            })
+            .catch(err => {
+                console.log("Error:", err.message);
+            });
+    }
+	
+	
+	
+    handleCalc = () => {
+        /*if (this.state.fromCurrency !== this.state.toCurrency) {
+            axios
+                //.get(`https://openexchangerates.org/api/latest?base=${this.state.fromCurrency}&symbols=${this.state.toCurrency}`)
+				.get('https://api.currencybeacon.com/' + endpoint + '?api_key=' + api_key +'&from=' + this.state.fromCurrency + '&to=' + this.state.toCurrency + '&amount=' + this.state.amount)
+                .then(response => {
+                    const result = this.state.amount * (response.data.rates[this.state.toCurrency]);
+                    this.setState({ result: result.toFixed(3) })
+                })
+                .catch(err => {
+                    console.log("Error:", err.message);
+                });
+        } */
 
-	const handleCalc = () => { //updates output box
 		
-		if (input !== '') {
-			if (country1 !== country2) { //countries must not be the same
-				
-				if (country1 !== "") { 
-					if (country2 !== "") {
+		let from = this.state.fromCurrency;
+		let to = this.state.toCurrency;
+		let amount = this.state.amount;
 
-
-
-
-
-
-						output =  input / 1.609344; // d(mi) = d(km) / 1.609344 <- https://www.rapidtables.com/ conversion
-						output = Math.round(output); //round temp as whole number
-
-						if (output !== 0) {
-
-							setOutput(output);
-						}
-						
-					}
-					//console.log("Error"); //else put out an error or don't do anything
-				}
-
-				/*if (unit1 === "mph") { //convert from mi to km
-					if (unit2 === "km/h") {
-						output =  input * 1.609344; // d(km) = d(mi) × 1.609344
-						output = Math.round(output);
-
-						if (output !== 0) {
-
-							setOutput(output);							
-						}
-
-					}
-					
-					//console.log("Error"); //else statement
-				}
-				*/
-				//console.log("Error");
+		
+		$.ajax({
+			url: 'https://api.currencybeacon.com/v1/convert?api_key=' + api_key +'&from=' + from + '&to=' + to +
+			'&amount=' + amount,
+			dataType: 'json',
+			success: function(json) {
+			
+			console.log("From: " + from);
+			console.log("To: " + to);
+			console.log("Amt: " + amount);
+			
+			// access the conversion result in json.result
+			console.log(json.result);
+			
 			}
+		});
+		
 
-			/*else {
-				console.log("Error"); // if both units are the same
 
-			}*/
-		}
-
-		/*else {
-			console.log("Error"); // <- Maybe use only this and create a nested if statement?
-		}
-		*/
 		
 		
-		//TODO: do error checking - issue an alert to user that units should not be same or just don't do anything		
+    };
+
+	render() {
+
+		return (
+			<div className="rect">
+				<h1>Currency</h1>
+
+				<input id="box1" name="amount" type="number" min="0" value={this.state.amount} onChange={ e => this.setState({ amount: e.target.value }) }/>
+
+				<Select
+					className="dropdown1"
+					//name="from"
+					placeholder="Country/Territory"
+					options={Countries}
+					val={this.state.fromCurrency}
+					//onChange={opt => setCountry1(opt.label.substring(opt.label.length - 4, opt.label.length - 1), opt.value)} //first part extracts the currency for the country/territory
+					//onChange={opt => console.log(opt.label, opt.value.substring(opt.value.length - 4, opt.value.length - 1))} 
+					
+					
+					//onChange={(e) => this.handleCurrencyChange(e)}
+					//onChange={(e) => console.log(e)} //this passes both the label and value in e
+					//onChange={(e) => console.log(e.value.substring(e.value.length - 4, e.value.length - 1))}
+					//onChange={(e) => console.log(this.handleCurrencyChange( e.value.substring(e.value.length - 4, e.value.length - 1)) )}  //extracting the currency for the country/territory selected
+
+					//onChange={(e) => this.handleCurrencyChange( e.value.substring(e.value.length - 4, e.value.length - 1) ) }  //extracting the currency for the country/territory selected
+					//onChange = {this.setState({ fromCurrency: e.target.value }) }
+					
+					onChange = {(e) => this.setState({ fromCurrency:  e.value.substring(e.value.length - 4, e.value.length - 1) }) }					
+					//onChange = {(e) => console.log(this.setState({ fromCurrency:  e.value.substring(e.value.length - 4, e.value.length - 1) })) }
+
+                    
+					noOptionsMessage={({ inputValue }) => `No result found for "${inputValue}"`}
+				/>
+			
+
+
+				<IconButton className="calc" onClick={this.handleCalc} >
+					<SwapHorizIcon fontSize="large"/>
+				</IconButton>
+
+
+				<input id="box2" value={this.state.result} type="number" />
+
+				<Select
+					className="dropdown2"
+					//name="to"
+					placeholder="Country/Territory"
+					options={Countries}
+					val={this.state.toCurrency}
+
+					//onChange={opt => setCountry2(opt.label, opt.value)}
+					//onChange={opt => console.log(opt.label.substring(opt.label.length - 4, opt.label.length - 1), opt.value)} 
+					
+					
+					//onChange={(e) => this.handleCurrencyChange(e)}
+					//onChange={(e) => this.handleCurrencyChange( e.value.substring(e.value.length - 4, e.value.length - 1)) }  //extracting the currency for the country/territory selected
+					//onChange={(e) => console.log(this.handleCurrencyChange( e.value.substring(e.value.length - 4, e.value.length - 1) ))}
+					
+					onChange = {(e) => this.setState({ toCurrency :  e.value.substring(e.value.length - 4, e.value.length - 1) }) }					
+					//onChange = {(e) => console.log(this.setState({ toCurrency :  e.value.substring(e.value.length - 4, e.value.length - 1) })) }
+
+
+					noOptionsMessage={({ inputValue }) => `No result found for "${inputValue}"`}
+				/>
+
+				<h3>{this.state.fromCurrency}, {this.state.toCurrency}</h3>
+
+			</div>
+		);
 	};
 
-	const ClearFields = () => {	
-		let input = document.getElementById("box1"); //clear input field when onfocus event is called
-		input.value = '';
+}
 
-		setOutput(''); //clear output
-	}
-
-	return (
-		<div className="rect">
-			<h1>Currency</h1>
-
-			<input id="box1" type="number" onFocus={ClearFields} min="0" onChange={e => setInput(e.target.value)}/>
-
-			<Select
-				className="dropdown1"
-				placeholder="Country/Territory"
-				options={Countries}
-				onChange={opt => setCountry1(opt.label, opt.value)}
-				noOptionsMessage={({ inputValue }) => `No result found for "${inputValue}"`}
-			/>
-
-
-			<IconButton className="calc" onClick={() => handleCalc()}>
-				<SwapHorizIcon fontSize="large"/>
-			</IconButton>
-
-
-			<input id="box2" value={output} onChange={e => setOutput(e.target.value)} type="number" />
-
-			<Select
-				className="dropdown2"
-				placeholder="Country/Territory"
-				options={Countries}
-				onChange={opt => setCountry2(opt.label, opt.value)}
-				noOptionsMessage={({ inputValue }) => `No result found for "${inputValue}"`}
-			/>
-
-		</div>
-	);
-};
+//
 
 export default Currency;
